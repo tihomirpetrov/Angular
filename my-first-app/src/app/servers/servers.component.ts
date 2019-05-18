240ba8c3c7e9b6fc457e2650053fad5346486ec6
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-05-18 14:59:24 +0300

    initial commit

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  //selector: '[app-servers]',
-  selector: '.app-servers',
+  selector: 'app-servers',
   template: `
     <app-server></app-server>
     <app-server></app-server>`,
@@ -10,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  constructor() {  }
 
   ngOnInit() {
   }
