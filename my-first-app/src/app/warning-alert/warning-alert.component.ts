e73a94db3a12d804b7cb510809ea21ac712a3e42
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-05-20 15:02:32 +0300

    initial commit

--- my-first-app/src/app/warning-alert/warning-alert.component.ts
+++ my-first-app/src/app/warning-alert/warning-alert.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-warning-alert',
   template: `
     <p>This is a warning, you are in danger!</p>
-  `
+  `,
+  styles: [
+      `
+      p {
+        padding: 20px;
+        background-color: mistyrose;
+      }
+    `
+  ]
 })
 export class WarningAlertComponent {
 
