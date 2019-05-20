afd93b85e44dc99984cf7d39b60ecaac0b91be62
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-05-20 16:57:04 +0300

    initial commit

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -7,13 +7,15 @@ import {AppComponent} from './app.component';
 import {ServerComponent} from './server/server.component';
 import {SuccessAlertComponent} from './success-alert/success-alert.component';
 import {WarningAlertComponent} from './warning-alert/warning-alert.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     WarningAlertComponent,
-    SuccessAlertComponent
+    SuccessAlertComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
