03a4b326ac6b22928642bb701532115709552807
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-06-11 22:01:14 +0300

    initial commit

--- tour-of-heroes/src/app/app.module.ts
+++ tour-of-heroes/src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
-
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 
 @NgModule({
@@ -15,7 +13,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
   ],
   imports: [
     BrowserModule,
-	FormsModule    
+	  FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
