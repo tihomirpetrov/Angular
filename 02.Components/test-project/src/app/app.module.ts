0858ee9d510003d07fc277a84dbb9201691acda1
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-06-20 19:00:26 +0300

    initial commit

--- 02.Components/test-project/src/app/app.module.ts
+++ 02.Components/test-project/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { GameComponent } from './game/game.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { GameComponent } from './game/game.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
