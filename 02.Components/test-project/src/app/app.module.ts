d135d5d8e2e4221fba07f89d50e3dec2a82a1fa5
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-06-20 17:16:00 +0300

    initial commit

--- 02.Components/test-project/src/app/app.module.ts
+++ 02.Components/test-project/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { GameComponent } from './game/game.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
---
+++ 02.Components/test-project/src/app/game/game.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {Game} from "../domain/game";
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.css']
+})
+export class GameComponent implements OnInit {
+  games: Array<Game>;
+  constructor() {
+    this.games = [{id: 1, title: "Contra", image: "https://media0.mensxp.com/media/content/2013/May/1369382418_61189.jpg"}, {id: 2, title: "Super Mario", image: "https://i.kinja-img.com/gawker-media/image/upload/s--qPViLv1T--/c_scale,f_auto,fl_progressive,q_80,w_800/17g98i3kcj6zpjpg.jpg"}]
+  }
+
+  ngOnInit() {
+  }
+
+}
