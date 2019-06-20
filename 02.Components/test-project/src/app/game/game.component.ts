c29bcdd1f6ec60f9b2bdf29a9914f6c464ec3f40
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-06-20 18:50:11 +0300

    initial commit

--- 02.Components/test-project/src/app/game/game.component.ts
+++ 02.Components/test-project/src/app/game/game.component.ts
@@ -9,6 +9,7 @@ import {Game} from "../domain/game";
 export class GameComponent implements OnInit {
   games: Array<Game>;
   isShown: boolean = false;
+  name_user: string;
   constructor() {
     this.games = [{id: 1, title: "Contra", image: "https://media0.mensxp.com/media/content/2013/May/1369382418_61189.jpg"}, {id: 2, title: "Super Mario", image: "https://i.kinja-img.com/gawker-media/image/upload/s--qPViLv1T--/c_scale,f_auto,fl_progressive,q_80,w_800/17g98i3kcj6zpjpg.jpg"}]
   }
@@ -18,6 +19,7 @@ export class GameComponent implements OnInit {
   }
 
   showName(name){
+    this.name_user = name;
     console.log(name);
   }
 
