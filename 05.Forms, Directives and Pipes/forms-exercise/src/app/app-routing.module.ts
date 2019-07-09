6c28f15ddbd783bacc29a927ff3837e6bd80d317
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-07-09 19:36:17 +0300

    initial commit

--- 05.Forms, Directives and Pipes/forms-exercise/src/app/app-routing.module.ts
+++ 05.Forms, Directives and Pipes/forms-exercise/src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./authentication/login/login.component";
 import { RegisterComponent } from "./authentication/register/register.component";
+import {AuthGuard} from "./guards/auth.guard";
 
 const routes: Routes = [
-  {path: 'home', component : HomeComponent},
+  {path: 'home', component : HomeComponent, canActivate: [ AuthGuard ]},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: '', component : HomeComponent}
