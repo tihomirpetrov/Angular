daa291c09f74ce2a8e6d00f7787c691c378f4871
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-07-09 18:25:49 +0300

    initial commit

--- 05.Forms, Directives and Pipes/forms-exercise/src/app/authentication/register/register.component.ts
+++ 05.Forms, Directives and Pipes/forms-exercise/src/app/authentication/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RegisterModel } from "../../models/register.model";
 import {AuthService} from "../auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-register',
@@ -9,8 +10,10 @@ import {AuthService} from "../auth.service";
 })
 export class RegisterComponent implements OnInit {
   form: RegisterModel;
+  loginFailed : boolean;
+  errorMessage : string;
 
-  constructor( private authService : AuthService) {
+  constructor( private authService : AuthService, private router : Router) {
     this.form =  new RegisterModel('','','','','','', 18);
   }
 
@@ -19,7 +22,16 @@ export class RegisterComponent implements OnInit {
 
   registration() {
     delete this.form['confirmPassword'];
-    console.log(this.form);
+    this.authService.register(this.form)
+      .subscribe(
+        data => {
+          this.router.navigate(['/login'])
+       },
+        err =>{
+          console.log(err);
+        }
+      )
+    //console.log(this.form);
   }
 
   get diagnostic() { return JSON.stringify(this.form); }
