343b9ef0711c84e308f13ae6c39e9f58170a31cf
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-07-12 18:24:40 +0300

    initial commit

--- 06.Furniture System – Exercise/furniture-system/src/app/furniture/create-furniture/create-furniture.component.ts
+++ 06.Furniture System – Exercise/furniture-system/src/app/furniture/create-furniture/create-furniture.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-create-furniture',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-furniture.component.css']
 })
 export class CreateFurnitureComponent implements OnInit {
+  form : FormGroup ;
 
-  constructor() { }
+  constructor(private  fb: FormBuilder) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      make: ['', [Validators.required, Validators.minLength(4)]],
+      model: ['', [Validators.required, Validators.minLength(4)]],
+      year: ['', [Validators.required, Validators.min(1950), Validators.max(2050)]],
+      description: ['', [Validators.required, Validators.minLength(10)]],
+      price: ['', [Validators.required, Validators.min(0.1)]],
+      imageUrl: ['', Validators.required],
+      material: ['', Validators.nullValidator]
+    })
   }
 
+  createFurniture() {
+    console.log(this.form.value);
+  }
+
+  get f(){
+    return this.form.controls
+  }
 }
