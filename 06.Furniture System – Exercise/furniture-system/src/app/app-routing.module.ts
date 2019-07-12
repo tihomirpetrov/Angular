2fc8139f74c10745015e13060b69a709754a0b47
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-07-12 16:53:52 +0300

    initial commit

---
+++ 06.Furniture System – Exercise/furniture-system/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+// Components
+import { HomeComponent } from './home/home.component';
+import { SigninComponent } from './authentication/signin/signin.component';
+import { SignupComponent } from './authentication/signup/signup.component';
+import {CreateFurnitureComponent} from "./furniture/create-furniture/create-furniture.component";
+import {FurnitureAllComponent} from "./furniture/furniture-all/furniture-all.component";
+import {FurnitureUserComponent} from "./furniture/furniture-user/furniture-user.component";
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'signin', component: SigninComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'furniture/create', component: CreateFurnitureComponent},
+  { path: 'furniture/user', component: FurnitureUserComponent},
+  { path: 'furniture/all', component: FurnitureAllComponent}
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ 06.Furniture System – Exercise/furniture-system/src/app/app.module.ts
@@ -0,0 +1,54 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { SigninComponent } from './authentication/signin/signin.component';
+import { SignupComponent } from './authentication/signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import { DropdownDirective } from './navigation/dropdown.directive';
+import { CollapseDirective } from './navigation/collapse.directive';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AuthService } from './authentication/auth.service';
+import { ToastrModule } from "ngx-toastr";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { JwtInterceptorService } from "./jwt-interceptor.service";
+import { FurnitureAllComponent } from './furniture/furniture-all/furniture-all.component';
+import { FurnitureDetailsComponent } from './furniture/furniture-details/furniture-details.component';
+import { CreateFurnitureComponent } from './furniture/create-furniture/create-furniture.component';
+import { FurnitureUserComponent } from './furniture/furniture-user/furniture-user.component';
+import { FurnitureService } from "./furniture/furniture.service";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    SigninComponent,
+    SignupComponent,
+    HomeComponent,
+    DropdownDirective,
+    CollapseDirective,
+    FurnitureAllComponent,
+    FurnitureDetailsComponent,
+    CreateFurnitureComponent,
+    FurnitureUserComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule
+  ],
+  providers: [ 
+    AuthService,
+    FurnitureService,
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptorService, multi: true}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
